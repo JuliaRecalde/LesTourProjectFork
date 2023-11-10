7e5463ab53747677e87e0f5090731d97e3b059e5
JuliaRecalde <yuliantonia@gmail.com>
2023-11-10 16:13:50 -0300

    test urls de las vistas

--- lesTourWeb/tests.py
+++ lesTourWeb/tests.py
@@ -1,16 +1,37 @@
 from django.test import TestCase
 from django.test import reverse
-from lesTourWeb.views import hoteles, personal, usuarios
+from lesTourWeb.views import hoteles, personal, usuarios, Home, SignUp, Reservation, SignIn
+
+from django.test import TestCase
+from django.urls import reverse
 
 class PruebasDeURL(TestCase):
-    def Unit_Test1(self):
+    def test_hoteles_url(self):
         response = self.client.get(reverse('hoteles'))
         self.assertEqual(response.status_code, 200)
-        
-    def Unit_Test2(self):
+
+    def test_personal_url(self):
         response = self.client.get(reverse('personal'))
         self.assertEqual(response.status_code, 200)
-        
-    def Unit_Test3(self):
-            response = self.client.get(reverse('usuarios'))
-            self.assertEqual(response.status_code, 200)
+
+    def test_usuarios_url(self):
+        response = self.client.get(reverse('usuarios'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url(self):
+        response = self.client.get(reverse('Home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_url(self):
+        response = self.client.get(reverse('SingUp'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signin_url(self):
+        response = self.client.get(reverse('SingIn'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_reservation_url(self):
+        response = self.client.get(reverse('Reservation'))
+        self.assertEqual(response.status_code, 200)
+
+##test para el modelo Usuarios
